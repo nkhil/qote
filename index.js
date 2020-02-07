2db87f9ac4961ca4cea365f13af9147ebaff29ce
Nikhil <nikhil@jigsaw.xyz>
2020-02-07 23:38:54 +0000

    :sparkles: first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const quot = require('./src');
+
+module.exports = {
+  ...quot,
+};
---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function get() {
+  return {
+    quote: 'Imagination is the only weapon in the war against reality',
+    author: 'Lewis Carroll',
+  };
+}
+
+module.exports = {
+  get,
+};
