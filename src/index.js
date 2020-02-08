6fc78a586f3cb9048f72e69dd127d4c85e218b92
Nikhil Vijayan <nkhilv@gmail.com>
2020-02-08 00:03:33 +0000

    :pencil: add quote

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@
 function get() {
   return {
     quote: 'Imagination is the only weapon in the war against reality',
-    author: 'Lewis Carroll',
+    author: 'Jules de Gaultier',
+    source: 'https://www.goodreads.com/quotes/128249-imagination-is-the-only-weapon-in-the-war-against-reality'
   };
 }
 
