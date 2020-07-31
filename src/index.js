34a105798c179bb5180cbacb68f6654f987b21c5
Nikhil Vijayan <nkhilv@gmail.com>
2020-07-31 09:19:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 'use strict';
 
+const quotes = [
+  {
+    quoteText: 'Don\'t ask what the world needs. Ask what makes you come alive and go do it. Because what the world needs is people who have come alive',
+    quoteAuthor: 'Howard Thurman',
+    source: '',
+    tags: ['passion'],
+  } 
+]
+
 function get() {
   return {
     quote: 'Imagination is the only weapon in the war against reality',
